4196b07d1149e034989ac06457da35713b53d483
yk-kasshi <19880601.yuki@gmail.com>
2023-03-21 15:39:46 +0900

    Revert "Merge pull request #8 from yk-kasshi/second_training"
    This reverts commit d0a9e2bfbaaa6234d3fc0bcb226184426644ecf1, reversing
    changes made to 1acf9f1f67ce6db4fb6bc73edc1db192ad52758f.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -33,15 +33,10 @@ class CalendarsController < ApplicationController
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
-
-
-      wday_num = Date.today.wday + x
-      if wday_num >= 7
-        wday_num = wday_num -7
       end
-      
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num] }
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
+
   end
 end
